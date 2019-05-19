b77a4caf58dfb832c26525083ca5db0b6bf7183a
Jon Deaton <jonpauldeaton@gmail.com>
2019-05-19 00:05:01 -0700

    added some EGL code...

--- agario/rendering/FrameBufferObject.hpp
+++ agario/rendering/FrameBufferObject.hpp
@@ -5,6 +5,28 @@
 
 #include "rendering/Canvas.hpp"
 
+#ifdef USE_EGL
+#include <EGL/egl.h>
+static const EGLint configAttribs[] = {
+        EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+        EGL_BLUE_SIZE, 8,
+        EGL_GREEN_SIZE, 8,
+        EGL_RED_SIZE, 8,
+        EGL_DEPTH_SIZE, 8,
+        EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT,
+        EGL_NONE
+};
+
+static const int pbufferWidth = 9;
+static const int pbufferHeight = 9;
+
+static const EGLint pbufferAttribs[] = {
+      EGL_WIDTH, pbufferWidth,
+      EGL_HEIGHT, pbufferHeight,
+      EGL_NONE,
+};
+#endif
+
 class FBOException : public std::runtime_error {
   using runtime_error::runtime_error;
 };
@@ -24,6 +46,48 @@ public:
     fbo(0), rbo_depth(0), rbo_color(0),
     window(nullptr) {
 
+#ifdef USE_EGL
+    _initialize_egl()
+#else
+    _initialize_context();
+#endif
+    _initialize_frame_buffers();
+  }
+
+  int width() const override { return _width; }
+  int height() const override { return _height; }
+
+  void show() const { glfwShowWindow(window); }
+  void hide() const { glfwHideWindow(window); }
+
+  void copy(void *data) {
+    glReadBuffer(GL_COLOR_ATTACHMENT0);
+    glReadPixels(_width / 2, _height / 2, _width, _height, GL_RGB, GL_UNSIGNED_BYTE, data);
+  }
+
+  void swap_buffers() const { glfwSwapBuffers(window); }
+
+  ~FrameBufferObject() override {
+    glDeleteFramebuffers(1, &fbo);
+    glDeleteRenderbuffers(1, &rbo_color);
+    glDeleteRenderbuffers(1, &rbo_depth);
+
+#ifdef USE_EGL
+    eglTerminate(eglDpy);
+#endif
+  }
+
+private:
+  const screen_len _width;
+  const screen_len _height;
+
+  GLuint fbo;
+  GLuint rbo_depth;
+  GLuint rbo_color;
+
+  GLFWwindow *window;
+
+  void _initialize_context() {
     glfwSetErrorCallback(glfw_error_callback);
 
     if (!glfwInit())
@@ -46,7 +110,9 @@ public:
 
     glfwHideWindow(window);
     glfwMakeContextCurrent(window);
+  }
 
+  void _initialize_frame_buffers() {
     // Frame Buffer Object
     glGenFramebuffers(1, &fbo);
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
@@ -77,32 +143,32 @@ public:
     glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
   }
 
-  int width() const override { return _width; }
-  int height() const override { return _height; }
+#ifdef USE_EGL
+  void _initialize_egl() {
+    EGLDisplay eglDpy = eglGetDisplay(EGL_DEFAULT_DISPLAY);
 
-  void show() const { glfwShowWindow(window); }
-  void hide() const { glfwHideWindow(window); }
+    EGLint major, minor;
 
-  void copy(void *data) {
-    glReadBuffer(GL_COLOR_ATTACHMENT0);
-    glReadPixels(_width / 2, _height / 2, _width, _height, GL_RGB, GL_UNSIGNED_BYTE, data);
+    eglInitialize(eglDpy, &major, &minor);
+
+    // 2. Select an appropriate configuration
+    EGLint numConfigs;
+    EGLConfig eglCfg;
+
+    eglChooseConfig(eglDpy, configAttribs, &eglCfg, 1, &numConfigs);
+
+    // 3. Create a surface
+    EGLSurface eglSurf = eglCreatePbufferSurface(eglDpy, eglCfg,
+                                                 pbufferAttribs);
+
+    // 4. Bind the API
+    eglBindAPI(EGL_OPENGL_API);
+
+    // 5. Create a context and make it current
+    EGLContext eglCtx = eglCreateContext(eglDpy, eglCfg, EGL_NO_CONTEXT,
+                                         NULL);
+
+    eglMakeCurrent(eglDpy, eglSurf, eglSurf, eglCtx);
   }
-
-  void swap_buffers() const { glfwSwapBuffers(window); }
-
-  ~FrameBufferObject() override {
-    glDeleteFramebuffers(1, &fbo);
-    glDeleteRenderbuffers(1, &rbo_color);
-    glDeleteRenderbuffers(1, &rbo_depth);
-  }
-
-private:
-  const screen_len _width;
-  const screen_len _height;
-
-  GLuint fbo;
-  GLuint rbo_depth;
-  GLuint rbo_color;
-
-  GLFWwindow *window;
+#endif
 };
